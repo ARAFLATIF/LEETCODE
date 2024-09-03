f2a7cb47d974fde57649d29d6db523d7bd98c93e
Araf Ibne Latif <124219901+ARAFLATIF@users.noreply.github.com>
2024-09-03 14:03:37 -0500

    Update main.py

--- Duplicate Integer/main.py
+++ Duplicate Integer/main.py
@@ -25,3 +25,4 @@ class Solution:
 # Check each number in the list.
 # If it finds a number that's already in seen, it returns True (we've found a duplicate).
 # If it doesn't find any duplicates after checking all numbers, it returns False.
+#always remeber to check the name that it is using, like "has duplicate" and "contains duplicate"
